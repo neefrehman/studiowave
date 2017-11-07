f446a3814d2ae62e7ba407e8e3e191cbc128e005
Neef Rehman <neef.rehman@gmail.com>
2017-11-07 11:02:05 +0000

    JS arrays

---
+++ javascript/beats.js
@@ -0,0 +1,70 @@
+// Collect all the beats into an array
+
+const beatArray = [
+  {
+    "title": "Lifted (Chillwave - Retrowave - Synthwave Mix)",
+    "url": "https://www.youtube.com/watch?v=3ZdSDUyxFmc"
+    "id": "1"
+  },
+  {
+    "title": "24/7 Lofi Hiphop Radio",
+    "url": "https://www.youtube.com/watch?v=ezvTXN6vXRM"
+    "id": "2"
+  },
+  {
+    "title": "24/7 lofi hip hop radio",
+    "url": "https://www.youtube.com/watch?v=6rReMbO42uE"
+    "id": "3"
+  },
+  {
+    "title": "Tycho - Dive",
+    "url": "https://www.youtube.com/watch?v=Z6ih1aKeETk"
+    "id": "4"
+  },
+  {
+    "title": "Turquoise Memories - Glowing Dreams Mix",
+    "url": "https://www.youtube.com/watch?v=fQ5cb7A1mjA"
+    "id": "5"
+  },
+  {
+    "title": "Future Vision",
+    "url": "https://www.youtube.com/watch?v=prHFVUaL_bU"
+    "id": "6"
+  },
+  {
+    "title": "Lofi Hip Hop Radio 24/7",
+    "url": "https://www.youtube.com/watch?v=AQBh9soLSkI"
+    "id": "7"
+  },
+  {
+    "title": "Home - Odyssey",
+    "url": "https://www.youtube.com/watch?v=gJOfuGj-n9M"
+    "id": "8"
+  },
+  {
+    "title": "Jon Hopkins - Open Eye Signal",
+    "url": "https://www.youtube.com/watch?v=Q04ILDXe3QE"
+    "id": "9"
+  },
+  {
+    "title": "Vessels - Dilate",
+    "url": "https://www.youtube.com/watch?v=nT6NWc3J0-o&list=PLJpTOskHtiumaSrLawxxNSyHLgauGl85u"
+    "id": "10"
+  },
+];
+
+function shufflePortfolioArray() {
+
+  const a = portfolioArray;
+
+  var j, x, i;
+  for (i = a.length; i; i--) {
+    j = Math.floor(Math.random() * i);
+    x = a[i - 1];
+    a[i - 1] = a[j];
+    a[j] = x;
+  }
+
+  return a;
+
+}
---
+++ javascript/speeches.js
@@ -0,0 +1,115 @@
+// Collect all the beats into an array
+
+const speechArray = [
+  {
+    "title": "Charlie Chaplin - The Great Dictator Speech",
+    "url": "https://www.youtube.com/watch?v=J7GY1Xg6X20"
+    "id": "1"
+  },
+  {
+    "title": "T S Eliot reads his Four Quartets",
+    "url": "https://www.youtube.com/watch?v=Ga8tQrG4ZSw"
+    "id": "2"
+  },
+  {
+    "title": "JFK - The Speech That Killed Him",
+    "url": "https://www.youtube.com/watch?v=y8HTr-F-FVM"
+    "id": "3"
+  },
+  {
+    "title": "Steve Jobs' 2005 Stanford Commencement Address",
+    "url": "https://www.youtube.com/watch?v=UF8uR6Z6KLc"
+    "id": "4"
+  },
+  {
+    "title": "scent of a woman",
+    "url": "https://www.youtube.com/watch?v=TuYhfCkRxyE"
+    "id": "5"
+  },
+  {
+    "title": "Full Metal Jacket Opening",
+    "url": "https://www.youtube.com/watch?v=3j3_iPskjxk"
+    "id": "6"
+  },
+  {
+    "title": "Pale Blue Dot",
+    "url": "https://www.youtube.com/watch?v=-pvxKdvuwIw"
+    "id": "7"
+  },
+  {
+    "title": "University of Texas Address - Admiral McRaven",
+    "url": "https://www.youtube.com/watch?v=pxBQLFLei70"
+    "id": "8"
+  },
+  {
+    "title": "Richard Burton - Under Milk Wood",
+    "url": "https://www.youtube.com/watch?v=WJtzOD3KbLM"
+    "id": "9"
+  },
+  {
+    "title": "Eleanor Roosevelt Speech Human Rights",
+    "url": "https://www.youtube.com/watch?v=sPVWmmVKVk0"
+    "id": "10"
+  },
+  {
+    "title": "Winston S Churchill: We Shall Fight on the Beaches",
+    "url": "https://www.youtube.com/watch?v=MkTw3_PmKtc"
+    "id": "11"
+  },
+  {
+    "title": "Mahatma Gandhi Speech",
+    "url": "https://www.youtube.com/watch?v=_SakitCoNYc"
+    "id": "12"
+  },
+  {
+    "title": "Leonard Nimoy: College of Fine Arts Convocation Address",
+    "url": "https://www.youtube.com/watch?v=A26MnVnL8K4"
+    "id": "13"
+  },
+  {
+    "title": "The Lord of the Rings Audiobook Part 1",
+    "url": "https://www.youtube.com/watch?v=aazxRl5b7Ms"
+    "id": "14"
+  },
+  {
+    "title": "Jim Rohn Learn To Master These Skills",
+    "url": "https://www.youtube.com/watch?v=5ZBuWVvQMn8"
+    "id": "15"
+  },
+  {
+    "title": "Frost/Nixon: The Watergate Interviews",
+    "url": "https://www.youtube.com/watch?v=9OCOn3yKmwQ"
+    "id": "16"
+  },
+  {
+    "title": "Steve Jobs - THE MAJOR THINKERS",
+    "url": "https://www.youtube.com/watch?v=VnNBbR-JeDQ"
+    "id": "17"
+  },
+  {
+    "title": "2004 Barack Obama Keynote Speech",
+    "url": "https://www.youtube.com/watch?v=_fMNIofUw2I"
+    "id": "18"
+  },
+  {
+    "title": "Earl Nightingale The Dean of Personal Development",
+    "url": "https://www.youtube.com/watch?v=Bp8aP6pJB1Y"
+    "id": "19"
+  },
+];
+
+function shufflePortfolioArray() {
+
+  const a = portfolioArray;
+
+  var j, x, i;
+  for (i = a.length; i; i--) {
+    j = Math.floor(Math.random() * i);
+    x = a[i - 1];
+    a[i - 1] = a[j];
+    a[j] = x;
+  }
+
+  return a;
+
+}
