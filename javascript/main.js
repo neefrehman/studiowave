1063d5a795615db603d01b743423aa7fd6a3ba7f
Neef Rehman <neef.rehman@gmail.com>
2017-11-08 14:00:28 +0000

    :)

--- javascript/main.js
+++ javascript/main.js
@@ -3,188 +3,163 @@
 var beatPlayer;
 var speechPlayer;
 var isPlaying = false;
+var speechOrder;
+var beatOrder;
+var currentSpeechId;
+var currentBeatId;
 
 // MODEL
 
-const beatArray = [
-  {
+const beatArray = {
+  "3ZdSDUyxFmc": {
     "title": "Lifted (Chillwave - Retrowave - Synthwave Mix)",
     "id": "3ZdSDUyxFmc",
-    "url": "https://www.youtube.com/watch?v=3ZdSDUyxFmc",
-    "tag": "1"
+    "url": "https://www.youtube.com/watch?v=3ZdSDUyxFmc"
   },
-  {
+  "ezvTXN6vXRM": {
     "title": "24/7 Lofi Hiphop Radio",
     "id": "ezvTXN6vXRM",
-    "url": "https://www.youtube.com/watch?v=ezvTXN6vXRM",
-    "tag": "2"
+    "url": "https://www.youtube.com/watch?v=ezvTXN6vXRM"
   },
-  {
+  "6rReMbO42uE": {
     "title": "24/7 lofi hip hop radio",
     "id": "6rReMbO42uE",
-    "url": "https://www.youtube.com/watch?v=6rReMbO42uE",
-    "tag": "3"
+    "url": "https://www.youtube.com/watch?v=6rReMbO42uE"
   },
-  {
+  "Z6ih1aKeETk": {
     "title": "Tycho - Dive",
     "id": "Z6ih1aKeETk",
-    "url": "https://www.youtube.com/watch?v=Z6ih1aKeETk",
-    "tag": "4"
+    "url": "https://www.youtube.com/watch?v=Z6ih1aKeETk"
   },
-  {
+  "fQ5cb7A1mjA": {
     "title": "Turquoise Memories - Glowing Dreams Mix",
     "id": "fQ5cb7A1mjA",
-    "url": "https://www.youtube.com/watch?v=fQ5cb7A1mjA",
-    "tag": "5"
+    "url": "https://www.youtube.com/watch?v=fQ5cb7A1mjA"
   },
-  {
+  "prHFVUaL_bU": {
     "title": "Future Vision",
     "id": "prHFVUaL_bU",
-    "url": "https://www.youtube.com/watch?v=prHFVUaL_bU",
-    "tag": "6"
+    "url": "https://www.youtube.com/watch?v=prHFVUaL_bU"
   },
-  {
+  "AQBh9soLSkI": {
     "title": "Lofi Hip Hop Radio 24/7",
     "id": "AQBh9soLSkI",
-    "url": "https://www.youtube.com/watch?v=AQBh9soLSkI",
-    "tag": "7"
+    "url": "https://www.youtube.com/watch?v=AQBh9soLSkI"
   },
-  {
+  "gJOfuGj-n9M": {
     "title": "Home - Odyssey",
     "id": "gJOfuGj-n9M",
-    "url": "https://www.youtube.com/watch?v=gJOfuGj-n9M",
-    "tag": "8"
+    "url": "https://www.youtube.com/watch?v=gJOfuGj-n9M"
   },
-  {
+  "Q04ILDXe3QE": {
     "title": "Jon Hopkins - Open Eye Signal",
     "id": "Q04ILDXe3QE",
-    "url": "https://www.youtube.com/watch?v=Q04ILDXe3QE",
-    "tag": "9"
+    "url": "https://www.youtube.com/watch?v=Q04ILDXe3QE"
   },
-  {
+  "nT6NWc3J0-o": {
     "title": "Vessels - Dilate",
     "id": "nT6NWc3J0-o",
-    "url": "https://www.youtube.com/watch?v=nT6NWc3J0-o",
-    "tag": "10"
+    "url": "https://www.youtube.com/watch?v=nT6NWc3J0-o"
   }
-];
+};
 
-const speechesArray = [
-  {
+const speechArray = {
+  'J7GY1Xg6X20': {
     "title": "Charlie Chaplin - The Great Dictator Speech",
     "id": "J7GY1Xg6X20",
-    "url": "https://www.youtube.com/watch?v=J7GY1Xg6X20",
-    "tag": "1"
+    "url": "https://www.youtube.com/watch?v=J7GY1Xg6X20"
   },
-  {
+  'Ga8tQrG4ZSw': {
     "title": "T S Eliot reads his Four Quartets",
     "id": "Ga8tQrG4ZSw",
-    "url": "https://www.youtube.com/watch?v=Ga8tQrG4ZSw",
-    "tag": "2"
+    "url": "https://www.youtube.com/watch?v=Ga8tQrG4ZSw"
   },
-  {
+  'y8HTr-F-FVM': {
     "title": "JFK - The Speech That Killed Him",
     "id": "y8HTr-F-FVM",
-    "url": "https://www.youtube.com/watch?v=y8HTr-F-FVM",
-    "tag": "3"
+    "url": "https://www.youtube.com/watch?v=y8HTr-F-FVM"
   },
-  {
+  'UF8uR6Z6KLc': {
     "title": "Steve Jobs' 2005 Stanford Commencement Address",
     "id": "UF8uR6Z6KLc",
-    "url": "https://www.youtube.com/watch?v=UF8uR6Z6KLc",
-    "tag": "4"
+    "url": "https://www.youtube.com/watch?v=UF8uR6Z6KLc"
   },
-  {
+  'TuYhfCkRxyE': {
     "title": "scent of a woman",
     "id": "TuYhfCkRxyE",
-    "url": "https://www.youtube.com/watch?v=TuYhfCkRxyE",
-    "tag": "5"
+    "url": "https://www.youtube.com/watch?v=TuYhfCkRxyE"
   },
-  {
+  '3j3_iPskjxk': {
     "title": "Full Metal Jacket Opening",
     "id": "3j3_iPskjxk",
-    "url": "https://www.youtube.com/watch?v=3j3_iPskjxk",
-    "tag": "6"
+    "url": "https://www.youtube.com/watch?v=3j3_iPskjxk"
   },
-  {
+  '-pvxKdvuwIw': {
     "title": "Pale Blue Dot",
     "id": "-pvxKdvuwIw",
-    "url": "https://www.youtube.com/watch?v=-pvxKdvuwIw",
-    "tag": "7"
+    "url": "https://www.youtube.com/watch?v=-pvxKdvuwIw"
   },
-  {
+  'pxBQLFLei70': {
     "title": "University of Texas Address - Admiral McRaven",
     "id": "pxBQLFLei70",
-    "url": "https://www.youtube.com/watch?v=pxBQLFLei70",
-    "tag": "8"
+    "url": "https://www.youtube.com/watch?v=pxBQLFLei70"
   },
-  {
+  'WJtzOD3KbLM': {
     "title": "Richard Burton - Under Milk Wood",
     "id": "WJtzOD3KbLM",
-    "url": "https://www.youtube.com/watch?v=WJtzOD3KbLM",
-    "tag": "9"
+    "url": "https://www.youtube.com/watch?v=WJtzOD3KbLM"
   },
-  {
+  'sPVWmmVKVk0': {
     "title": "Eleanor Roosevelt Speech Human Rights",
     "id": "sPVWmmVKVk0",
-    "url": "https://www.youtube.com/watch?v=sPVWmmVKVk0",
-    "tag": "10"
+    "url": "https://www.youtube.com/watch?v=sPVWmmVKVk0"
   },
-  {
+  'MkTw3_PmKtc': {
     "title": "Winston S Churchill: We Shall Fight on the Beaches",
     "id": "MkTw3_PmKtc",
-    "url": "https://www.youtube.com/watch?v=MkTw3_PmKtc",
-    "tag": "11"
+    "url": "https://www.youtube.com/watch?v=MkTw3_PmKtc"
   },
-  {
+  '_SakitCoNYc': {
     "title": "Mahatma Gandhi Speech",
     "id": "_SakitCoNYc",
-    "url": "https://www.youtube.com/watch?v=_SakitCoNYc",
-    "tag": "12"
+    "url": "https://www.youtube.com/watch?v=_SakitCoNYc"
   },
-  {
+  'A26MnVnL8K4': {
     "title": "Leonard Nimoy: College of Fine Arts Convocation Address",
     "id": "A26MnVnL8K4",
-    "url": "https://www.youtube.com/watch?v=A26MnVnL8K4",
-    "tag": "13"
+    "url": "https://www.youtube.com/watch?v=A26MnVnL8K4"
   },
-  {
+  'aazxRl5b7Ms': {
     "title": "The Lord of the Rings Audiobook Part 1",
     "id": "aazxRl5b7Ms",
-    "url": "https://www.youtube.com/watch?v=aazxRl5b7Ms",
-    "tag": "14"
+    "url": "https://www.youtube.com/watch?v=aazxRl5b7Ms"
   },
-  {
+  '5ZBuWVvQMn8': {
     "title": "Jim Rohn Learn To Master These Skills",
     "id": "5ZBuWVvQMn8",
-    "url": "https://www.youtube.com/watch?v=5ZBuWVvQMn8",
-    "tag": "15"
+    "url": "https://www.youtube.com/watch?v=5ZBuWVvQMn8"
   },
-  {
+  '9OCOn3yKmwQ': {
     "title": "Frost/Nixon: The Watergate Interviews",
     "id": "9OCOn3yKmwQ",
-    "url": "https://www.youtube.com/watch?v=9OCOn3yKmwQ",
-    "tag": "16"
+    "url": "https://www.youtube.com/watch?v=9OCOn3yKmwQ"
   },
-  {
+  'VnNBbR-JeDQ': {
     "title": "Steve Jobs - THE MAJOR THINKERS",
     "id": "VnNBbR-JeDQ",
-    "url": "https://www.youtube.com/watch?v=VnNBbR-JeDQ",
-    "tag": "17"
+    "url": "https://www.youtube.com/watch?v=VnNBbR-JeDQ"
   },
-  {
+  '_fMNIofUw2I': {
     "title": "2004 Barack Obama Keynote Speech",
     "id": "_fMNIofUw2I",
-    "url": "https://www.youtube.com/watch?v=_fMNIofUw2I",
-    "tag": "18"
+    "url": "https://www.youtube.com/watch?v=_fMNIofUw2I"
   },
-  {
+  'Bp8aP6pJB1Y': {
     "title": "Earl Nightingale The Dean of Personal Development",
     "id": "Bp8aP6pJB1Y",
-    "url": "https://www.youtube.com/watch?v=Bp8aP6pJB1Y",
-    "tag": "19"
-  },
-];
+    "url": "https://www.youtube.com/watch?v=Bp8aP6pJB1Y"
+  }
+};
 
 
 // RangeSlider
@@ -234,7 +209,6 @@ $('#playpausebutton').on('click', function () {
     };
 
 });
-
 });
 
 
@@ -256,6 +230,10 @@ $('#speechreset').on('click', function () {
     }
 });
 
+$('#share').on('click', function () {
+  console.log(window.location.pathname + '?beat=' + currentBeatId + '&speech=' + currentSpeechId)
+});
+
 
 // if it is a agent detected mobile hideClass small_screen and show mobile_device
 
@@ -266,52 +244,76 @@ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
 }
 
 
-// Randomise video to be played
+// Shuffle
 
-function getRandomBeat() {
+function shuffle(array) {
+  var m = array.length, t, i;
 
-return beatArray[Math.floor(Math.random() * beatArray.length)];
+  // While there remain elements to shuffle…
+  while (m) {
 
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * m--);
+
+    // And swap it with the current element.
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+
+  return array;
 }
 
-function getRandomSpeech() {
 
-return speechesArray[Math.floor(Math.random() * speechesArray.length)];
-
+function onBeatStateChange() {
+  const index = beatPlayer.getPlaylistIndex();
+  const key = beatOrder[index];
+  const object = beatArray[key];
+  currentBeatId = key;
+  var BeatTitle = document.getElementById("BeatTitle");
+  BeatTitle.innerHTML = object.title;
+  BeatTitle.href = object.url;
 }
 
+function onSpeechStateChange() {
+  const index = speechPlayer.getPlaylistIndex();
+  const key = speechOrder[index];
+  const object = speechArray[key];
+  currentSpeechId = key;
+  var SpeechTitle = document.getElementById("SpeechTitle");
+  SpeechTitle.innerHTML = object.title;
+  SpeechTitle.href = object.url;
+}
 
 // Generate YouTube Players
 
 function onYouTubeIframeAPIReady() {
 
-    const speechId = getRandomSpeech().id;
-    console.log(speechId)
-    const beatId = getRandomBeat().id;
-    console.log(beatId)
+    beatOrder = shuffle(Object.keys(beatArray));
+    speechOrder = shuffle(Object.keys(speechArray));
 
     beatPlayer = new YT.Player('beatvideo', {
         width: 60,
         height: 40,
-        videoId: beatId,
         playerVars: {
             color: 'white',
-            playlist: 'gJOfuGj-n9M,Z6ih1aKeETk,prHFVUaL_bU,6rReMbO42uE'
+            playlist: beatOrder.join(',')
         },
         events: {
-            onReady: beatInitialized
+            onReady: beatInitialized,
+            onStateChange: onBeatStateChange
         }
     });
     speechPlayer = new YT.Player('speechvideo', {
         width: 60,
         height: 40,
-        videoId: speechId,
         playerVars: {
             color: 'white',
-            playlist: 'sPVWmmVKVk0,Bp8aP6pJB1Y,A26MnVnL8K4,3j3_iPskjxk'
+            playlist: speechOrder.join(',')
         },
         events: {
-            onReady: speechInitialized
+            onReady: speechInitialized,
+            onStateChange: onSpeechStateChange
         }
     });
 }
