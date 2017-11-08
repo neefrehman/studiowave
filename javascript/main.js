d94c5ed8362e62c5d17b09cbe9ede62fda670748
Neef Rehman <neef.rehman@gmail.com>
2017-11-08 12:50:26 +0000

    :)

--- javascript/main.js
+++ javascript/main.js
@@ -9,151 +9,180 @@ var isPlaying = false;
 const beatArray = [
   {
     "title": "Lifted (Chillwave - Retrowave - Synthwave Mix)",
-    "url": "3ZdSDUyxFmc",
-    "id": "1"
+    "id": "3ZdSDUyxFmc",
+    "url": "https://www.youtube.com/watch?v=3ZdSDUyxFmc",
+    "tag": "1"
   },
   {
     "title": "24/7 Lofi Hiphop Radio",
-    "url": "ezvTXN6vXRM",
-    "id": "2"
+    "id": "ezvTXN6vXRM",
+    "url": "https://www.youtube.com/watch?v=ezvTXN6vXRM",
+    "tag": "2"
   },
   {
     "title": "24/7 lofi hip hop radio",
-    "url": "6rReMbO42uE",
-    "id": "3"
+    "id": "6rReMbO42uE",
+    "url": "https://www.youtube.com/watch?v=6rReMbO42uE",
+    "tag": "3"
   },
   {
     "title": "Tycho - Dive",
-    "url": "Z6ih1aKeETk",
-    "id": "4"
+    "id": "Z6ih1aKeETk",
+    "url": "https://www.youtube.com/watch?v=Z6ih1aKeETk",
+    "tag": "4"
   },
   {
     "title": "Turquoise Memories - Glowing Dreams Mix",
-    "url": "fQ5cb7A1mjA",
-    "id": "5"
+    "id": "fQ5cb7A1mjA",
+    "url": "https://www.youtube.com/watch?v=fQ5cb7A1mjA",
+    "tag": "5"
   },
   {
     "title": "Future Vision",
-    "url": "prHFVUaL_bU",
-    "id": "6"
+    "id": "prHFVUaL_bU",
+    "url": "https://www.youtube.com/watch?v=prHFVUaL_bU",
+    "tag": "6"
   },
   {
     "title": "Lofi Hip Hop Radio 24/7",
-    "url": "AQBh9soLSkI",
-    "id": "7"
+    "id": "AQBh9soLSkI",
+    "url": "https://www.youtube.com/watch?v=AQBh9soLSkI",
+    "tag": "7"
   },
   {
     "title": "Home - Odyssey",
-    "url": "gJOfuGj-n9M",
-    "id": "8"
+    "id": "gJOfuGj-n9M",
+    "url": "https://www.youtube.com/watch?v=gJOfuGj-n9M",
+    "tag": "8"
   },
   {
     "title": "Jon Hopkins - Open Eye Signal",
-    "url": "Q04ILDXe3QE",
-    "id": "9"
+    "id": "Q04ILDXe3QE",
+    "url": "https://www.youtube.com/watch?v=Q04ILDXe3QE",
+    "tag": "9"
   },
   {
     "title": "Vessels - Dilate",
-    "url": "nT6NWc3J0-o",
-    "id": "10"
+    "id": "nT6NWc3J0-o",
+    "url": "https://www.youtube.com/watch?v=nT6NWc3J0-o",
+    "tag": "10"
   }
 ];
 
 const speechesArray = [
   {
     "title": "Charlie Chaplin - The Great Dictator Speech",
-    "url": "J7GY1Xg6X20",
-    "id": "1"
+    "id": "J7GY1Xg6X20",
+    "url": "https://www.youtube.com/watch?v=J7GY1Xg6X20",
+    "tag": "1"
   },
   {
     "title": "T S Eliot reads his Four Quartets",
-    "url": "Ga8tQrG4ZSw",
-    "id": "2"
+    "id": "Ga8tQrG4ZSw",
+    "url": "https://www.youtube.com/watch?v=Ga8tQrG4ZSw",
+    "tag": "2"
   },
   {
     "title": "JFK - The Speech That Killed Him",
-    "url": "y8HTr-F-FVM",
-    "id": "3"
+    "id": "y8HTr-F-FVM",
+    "url": "https://www.youtube.com/watch?v=y8HTr-F-FVM",
+    "tag": "3"
   },
   {
     "title": "Steve Jobs' 2005 Stanford Commencement Address",
-    "url": "UF8uR6Z6KLc",
-    "id": "4"
+    "id": "UF8uR6Z6KLc",
+    "url": "https://www.youtube.com/watch?v=UF8uR6Z6KLc",
+    "tag": "4"
   },
   {
     "title": "scent of a woman",
-    "url": "TuYhfCkRxyE",
-    "id": "5"
+    "id": "TuYhfCkRxyE",
+    "url": "https://www.youtube.com/watch?v=TuYhfCkRxyE",
+    "tag": "5"
   },
   {
     "title": "Full Metal Jacket Opening",
-    "url": "3j3_iPskjxk",
-    "id": "6"
+    "id": "3j3_iPskjxk",
+    "url": "https://www.youtube.com/watch?v=3j3_iPskjxk",
+    "tag": "6"
   },
   {
     "title": "Pale Blue Dot",
-    "url": "-pvxKdvuwIw",
-    "id": "7"
+    "id": "-pvxKdvuwIw",
+    "url": "https://www.youtube.com/watch?v=-pvxKdvuwIw",
+    "tag": "7"
   },
   {
     "title": "University of Texas Address - Admiral McRaven",
-    "url": "pxBQLFLei70",
-    "id": "8"
+    "id": "pxBQLFLei70",
+    "url": "https://www.youtube.com/watch?v=pxBQLFLei70",
+    "tag": "8"
   },
   {
     "title": "Richard Burton - Under Milk Wood",
-    "url": "WJtzOD3KbLM",
-    "id": "9"
+    "id": "WJtzOD3KbLM",
+    "url": "https://www.youtube.com/watch?v=WJtzOD3KbLM",
+    "tag": "9"
   },
   {
     "title": "Eleanor Roosevelt Speech Human Rights",
-    "url": "sPVWmmVKVk0",
-    "id": "10"
+    "id": "sPVWmmVKVk0",
+    "url": "https://www.youtube.com/watch?v=sPVWmmVKVk0",
+    "tag": "10"
   },
   {
     "title": "Winston S Churchill: We Shall Fight on the Beaches",
-    "url": "MkTw3_PmKtc",
-    "id": "11"
+    "id": "MkTw3_PmKtc",
+    "url": "https://www.youtube.com/watch?v=MkTw3_PmKtc",
+    "tag": "11"
   },
   {
     "title": "Mahatma Gandhi Speech",
-    "url": "_SakitCoNYc",
-    "id": "12"
+    "id": "_SakitCoNYc",
+    "url": "https://www.youtube.com/watch?v=_SakitCoNYc",
+    "tag": "12"
   },
   {
     "title": "Leonard Nimoy: College of Fine Arts Convocation Address",
-    "url": "A26MnVnL8K4",
-    "id": "13"
+    "id": "A26MnVnL8K4",
+    "url": "https://www.youtube.com/watch?v=A26MnVnL8K4",
+    "tag": "13"
   },
   {
     "title": "The Lord of the Rings Audiobook Part 1",
-    "url": "aazxRl5b7Ms",
-    "id": "14"
+    "id": "aazxRl5b7Ms",
+    "url": "https://www.youtube.com/watch?v=aazxRl5b7Ms",
+    "tag": "14"
   },
   {
     "title": "Jim Rohn Learn To Master These Skills",
-    "url": "5ZBuWVvQMn8",
-    "id": "15"
+    "id": "5ZBuWVvQMn8",
+    "url": "https://www.youtube.com/watch?v=5ZBuWVvQMn8",
+    "tag": "15"
   },
   {
     "title": "Frost/Nixon: The Watergate Interviews",
-    "url": "9OCOn3yKmwQ",
-    "id": "16"
+    "id": "9OCOn3yKmwQ",
+    "url": "https://www.youtube.com/watch?v=9OCOn3yKmwQ",
+    "tag": "16"
   },
   {
     "title": "Steve Jobs - THE MAJOR THINKERS",
-    "url": "VnNBbR-JeDQ",
-    "id": "17"
+    "id": "VnNBbR-JeDQ",
+    "url": "https://www.youtube.com/watch?v=VnNBbR-JeDQ",
+    "tag": "17"
   },
   {
     "title": "2004 Barack Obama Keynote Speech",
-    "url": "_fMNIofUw2I",
-    "id": "18"
+    "id": "_fMNIofUw2I",
+    "url": "https://www.youtube.com/watch?v=_fMNIofUw2I",
+    "tag": "18"
   },
   {
     "title": "Earl Nightingale The Dean of Personal Development",
-    "url": "Bp8aP6pJB1Y",
-    "id": "19"
+    "id": "Bp8aP6pJB1Y",
+    "url": "https://www.youtube.com/watch?v=Bp8aP6pJB1Y",
+    "tag": "19"
   },
 ];
 
@@ -256,18 +285,18 @@ return speechesArray[Math.floor(Math.random() * speechesArray.length)];
 
 function onYouTubeIframeAPIReady() {
 
-    const speechURL = getRandomSpeech().url;
-    console.log(speechURL)
-    const beatURL = getRandomBeat().url;
-    console.log(beatURL)
+    const speechId = getRandomSpeech().id;
+    console.log(speechId)
+    const beatId = getRandomBeat().id;
+    console.log(beatId)
 
     beatPlayer = new YT.Player('beatvideo', {
         width: 60,
         height: 40,
-        videoId: beatURL,
+        videoId: beatId,
         playerVars: {
             color: 'white',
-            playlist: 'prHFVUaL_bU,6rReMbO42uE'
+            playlist: 'gJOfuGj-n9M,Z6ih1aKeETk,prHFVUaL_bU,6rReMbO42uE'
         },
         events: {
             onReady: beatInitialized
@@ -276,10 +305,10 @@ function onYouTubeIframeAPIReady() {
     speechPlayer = new YT.Player('speechvideo', {
         width: 60,
         height: 40,
-        videoId: speechURL,
+        videoId: speechId,
         playerVars: {
             color: 'white',
-            playlist: 'A26MnVnL8K4,3j3_iPskjxk'
+            playlist: 'sPVWmmVKVk0,Bp8aP6pJB1Y,A26MnVnL8K4,3j3_iPskjxk'
         },
         events: {
             onReady: speechInitialized
