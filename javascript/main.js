9857a550da11e54e86ca55165265d9562936fc1c
Neef Rehman <neef.rehman@gmail.com>
2020-02-16 15:54:27 +0000

    spreadsheet migration

--- javascript/main.js
+++ javascript/main.js
@@ -1,189 +1,4 @@
-// YT video arrays
-const beatArray = {
-    "3ZdSDUyxFmc": "Lifted (Chillwave/Retrowave Mix)",
-    "6rReMbO42uE": "24/7 lofi hip hop",
-    "Z6ih1aKeETk": "Tycho - Dive",
-    "fQ5cb7A1mjA": "Turquoise Memories - Glowing Dreams",
-    "prHFVUaL_bU": "Future Vision",
-    "AQBh9soLSkI": "Lofi HipHop Radio 24/7",
-    "gJOfuGj-n9M": "Home - Odyssey",
-    "Q04ILDXe3QE": "Jon Hopkins - Open Eye Signal",
-    "nT6NWc3J0-o": "Vessels - Dilate",
-    "QGpgAz__uXs": "Lounge Instrumental Chillout music",
-    "kPChs1S6s1E": "Lifted (Weekend Relaxation)",
-    "JSyOjpiL2AY": "Voyage (Chillwave/Retrowave/Synthwave Mix)",
-    "jLFd6k1BHDk": "Lifted (Mogwai - Auto Rock)",
-    "JzIK5FaC38w": "Lifted (Explosions In The Sky - Your Hand In Mine)",
-    "sQKdl_0cpJc": "Indecence - Next In Life",
-    "u5IR7nBU3C4": "Vessels - 4AM",
-    "wPjN5GNpCTI": "Vessels - Elliptic",
-    "mGBK8-u5h20": "Ulrich Schnauss - Far away trains passing by",
-    "F9L4q-0Pi4E": "２８１４ - 新しい日の誕生/Birth of a New Day",
-    "kFafKvgpIRo": "Lazerhawk - Visitors",
-    "kg9gjQnSCAw": "Tycho - Epoch",
-    "bR9-2As-XMk": "Kiasmos - Burn",
-    "cr4bsdoZKxQ": "Kiasmos - Thrown",
-    "3ycP6FvxYFo": "Kiasmos - Looped",
-    "WfYOMuX-tH8": "Stranger Things Soundtrack",
-    "ZPfNgIj2eNU": "Best of Tycho",
-    "u9TJ1L9VpZU": "Vessels - Attica",
-    "fi0KOScfBys": "Tycho - Sunrise Projector",
-    "Kr-xTnrxMq8": "Bing & Ruth - Tomorrow Was the Golden Age",
-    "vNwYtllyt3Q": "Brian Eno - Ambient 1: Music for Airports",
-    "7Cc-0beyRXE": "Huerco S. - Those Of You Who Have Never",
-    "_JxiJ9GX1yA": "Vessels - Mobilise",
-    "ggLh9P9x24Q": "Vessels - The Sky Was Pink",
-    "wOMwO5T3yT4": "ＳＰＡＣＥ ＴＲＩＰ - Retrowave Mix",
-    "Jmv5pTyz--I": "Stranger Things Theme Song (C418 REMIX)",
-    "WI4-HUn8dFc": "ＳＰＡＣＥ ＴＲＩＰ no. II - Retrowave Mix",
-    "1LHNmGe8HWk": "Perfect Day Legend Of Zelda Mix | Lo fi Hip Hop",
-    "YV9gP5xp7OE": "Night on Mountain | lofi hip hop |",
-    "T0nodan87dM": "Vaporwave~Hiphop~Vibes",
-    "-FlxM_0S2lA": "Lofi hip hop mix",
-    "uHaZzrUVnmw": "Hip-Hop Instrumentals Vol.2 - Aroto",
-    "GGBm9gTY2NU": "DownTown | Jazzy HipHop",
-    "tXQzAApb4WM": "Questlove - Goodbye Isaac",
-    "Agl1TgVfls0": "J Dilla - Life (Instrumental)",
-    "rSibMLeJYaE": "Hip Hop Jazz Chill - Need for Weekend",
-    "9ka5bgHnHyg": "Hip Hop Funk Jazz - Ours Samplus",
-    "V93yBHhxHTM": "Instrumental Hip Hop Buena Vista Chan Chan",
-    "Au46lJu8icA": "Old School Instrumental Hip Hop Session",
-    "_Rd2vKI6Amk": "Chill Instrumental Hiphop & JazzHop Mix",
-    "UHffJ0qC9Ko": "Gramatik Just Jammin",
-    "7ZguAEoNpZw": "L'indécis - Soulful",
-    "KjQSSYOTXzk": "Khruangbin - Friday Morning",
-    "e05rLVZQKiU": "Tommy Guerrero - No Mans Land ",
-    "zdYzL6wkr0A": "Time (Chillwave - Lofi - Electronic Mix)",
-    "ksaWk4ng_yA": "Hentzup - Out Of The Blue",
-    "gS62leDULLY": "Aso - Dreamer",
-    "6Y1naR7xDY8": "Saiko - In Space",
-    "kO43brxekLI": "Sounds Positive ☛ Jazz Oldschool HipHop",
-    "lSU1eFxgr68": "Vanilla - Origin (Full Album)",
-    "IFS0Eo1eh6Y": "Joey Bada$$ - Waves Instrumental",
-    "XG5umW9LTfo": "Flatbush ZOMBiES - Palm Trees",
-    "9eBfa9nNzvk": "DOOM & RZA - Books Of War",
-    "GJ-rORq34js": "J-Force - Pink Chicken '98",
-    "SU6x6nWQWI0": "Wio-K - We Nah Easy",
-    "7t2PhJUddIk": "Late June - You & I",
-    "1N8jKsu1NCE": "Julien Marchal - Insight XX",
-    "mYntXSsNnDQ": "Pete Rock - Lost Sessions",
-    "isIj3tuQTDY": "Glitch Mix"
-};
-
-const speechArray = {
-    "J7GY1Xg6X20": "Charlie Chaplin - The Great Dictator",
-    "Ga8tQrG4ZSw": "T S Eliot reads his Four Quartets",
-    "y8HTr-F-FVM": "JFK - The Speech That Killed Him",
-    "UF8uR6Z6KLc": "Steve Jobs' 2005 Stanford Address",
-    "9KvDvRUNw9k": "Simon Sinek - How to Find Fulfillment",
-    "3j3_iPskjxk": "Full Metal Jacket Opening",
-    "-pvxKdvuwIw": "Pale Blue Dot",
-    "pxBQLFLei70": "Admiral McRaven University of Texas Address",
-    "tY5It0jnD6M": "Black Mirror - 15 Million Merits",
-    "DLLihqJ22Vo": "Adaptation: Intro",
-    "xAhSc6akNPo": "A Lesson by Robert McKee on dialogue",
-    "PgU71nWCNeY": "Fight Club Speech",
-    "bivrs6stgHo": "'More Was Never Enough'",
-    "Nb9yu6dljAU": "There Will Be Blood - Milkshake",
-    "FkJLTGweKl0": "Rocky Balboa - speech to his son",
-    "WINDtlPXmmE": "Network - Mad as Hell",
-    "hopNAI8Pefg": "A Few Good Men",
-    "r0pOE8JlJbI": "Richard Dawkins - We are going to Die",
-    "0R3yJA7TnSk": "Sam Harris - Reality of your life is Now",
-    "cWXnT6Bwit4": "What If? - Jon Jorgenson",
-    "rOX1ETA0eUo": "No choices without chances - George the Poet",
-    "NZ4IoOXJiwQ": "The Most Astounding Fact - Neil deGrasse Tyson",
-    "WJtzOD3KbLM": "Richard Burton - Under Milk Wood",
-    "sPVWmmVKVk0": "Eleanor Roosevelt Speech Human Rights",
-    "MkTw3_PmKtc": "Winston S Churchill: Fight on the Beaches",
-    "_SakitCoNYc": "Mahatma Gandhi Speech",
-    "A26MnVnL8K4": "Leonard Nimoy College of Fine Arts Address",
-    "5ZBuWVvQMn8": "Jim Rohn Learn To Master These Skills",
-    "9OCOn3yKmwQ": "The Watergate Interviews",
-    "VnNBbR-JeDQ": "Steve Jobs - The Major Thinkers",
-    "_fMNIofUw2I": "2004 Barack Obama Keynote Speech",
-    "Bp8aP6pJB1Y": "The Dean of Personal Development",
-    "MOqIotJrFVM": "Malala Yousafzai Nobel Peace Prize Speech",
-    "oJJGuIZVfLM": "Severn Cullis-Suzuki at Rio Summit",
-    "Fg0mu32h5IY": "Maya Angelou - On the Pulse of Morning",
-    "ov4iyFoKcIs": "Dr Maya Angelou Love Liberates",
-    "keCwRdbwNQY": "Steve Jobs - Think different",
-    "UWzNsxRyvew": "Warren Buffett - How to Stay Out of Debt",
-    "5897dMWJiSM": "Marcus Aurelius - Lecture on Stoicism",
-    "UpiBUXXE8oU": "Earl Nightingale - Why do you go to work?",
-    "4IdEjAbH-LA": "Earl Nightingale - Imagination Is Everything",
-    "8PaAl51TV1o": "30 day Challenge - Earl Nightingale",
-    "_vmBQvYX8ig": "The Key to Happiness - Earl Nightingale",
-    "dFRpjk7seF8": "Sadhguru at TED",
-    "BkvEpoqFx6c": "Henry Rollins: The One Decision",
-    "fajfkO_X0l0": "Sam Harris: The Self is an Illusion",
-    "evM1NX3ebFE": "Conquer Life's Challenges - Cornel West",
-    "48wgDP9pc2E": "Oprah Winfrey - Power of Belief",
-    "7t_Uyi9bNS4": "Sam Harris on the Illusion of Free Will",
-    "ADeoLT0Dz2w": "Pat Mullins - The Problem With Rolling",
-    "GMWFieBGR7c": "Oprah Winfrey - Harvard Commencement",
-    "kn2Rp51Vpiw": "Bryan Cranston's Career Advice",
-    "gHbYJfwFgOU": "Bill Nye: Creationism Isn't Appropriate For Children",
-    "-ZRLe6jYu0s": "'Happiness isn't about what the world gives you'",
-    "Tc24RCmvlTU": "The Truth About Happiness",
-    "zWjuZ0OvtNU": "How To Train Your Brain To Stay Focused",
-    "CzSMC5rWvos": "Neil deGrasse Tyson: Atheist or Agnostic",
-    "ouRbkBAOGEw": "JFK - We choose to go to the Moon",
-    "IyArUh8eqJ0": "JFK/Eisenhower during Cuban Missile Crisis",
-    "0sV3miSCTG0": "Amelia Earhart On Women In Flying",
-    "aaiPkEhB0lY": "Audrey Tatou interview",
-    "bHOHi5ueo0A": "The laughing heart",
-    "MTPxWkBgW6U": "Charles Bukowski on dying and how to write",
-    "JqOqo50LSZ0": "Maya Angelou - And Still I Rise",
-    "CN9DN_PImy8": "Maya Angelou - The Mask",
-    "AlVQ7QffzjM": "Dougal Wilson on making music videos",
-    "5K_UAowTszU": "Richard Linklater on being self-taught ",
-    "T74EBhm5IFU": "Ellen Hovde & co. on documentaries",
-    "91uiHjHmEZc": "Ridley Scott on filmmaking",
-    "g9lBlD04sDs": "Aaron Draplin on Working with Free Vectors",
-    "v3vJ5Pvfbzk": "Draw Blindfolded - Shantell Martin",
-    "e80BbX05D7Y": "How To Begin Your Presentation - Simon Sinek",
-    "Ry94tFG4CIM": "Do Not Think Too Much - Alan Watts",
-    "kB5UCVW5zj4": "Alan Watts - Stop Negative Thoughts",
-    "kiSoxFHyjGY": "Dan Rather's account of JFK assasination",
-    "8-lLC4c_Mso": "Dan Rather Final Goodbye",
-    "PDv32Oo8PmE": "Justin Trudeau speech at Edinburgh University",
-    "xKCuDxpccYM": "Elon Musk's Break Down on Climate Change",
-    "MfXEnPO4R_s": "Ludacris Breaks Down His Favorite Rap Lyrics",
-    "b2mMX6vFc4s": "George the Poet - My City",
-    "iqm-XEqpayc": "Sheryl Sandberg Berkley Speech",
-    "1hlCEIUATzg": "Ellen Page - Time to Thrive Conference",
-    "c9SUAcNlVQ4": "Emma Watson UN address: I'm a Feminist",
-    "a7_49EXuLoQ": "Mindy Kalin on Growing Up",
-    "YlQzmwBiHdk": "Angelina Jolie Honors Agnes Varga",
-    "DVCfFBlKpN8": "Nora Ephron - Wellesley College Commencement",
-    "ZPCkfARH2eE": "Lupita Nyong'o on self-acceptance",
-    "xHHzHu289Ws": "Sarah Key: Private Parts",
-    "sXfrVdhmilI": "Amy Cuddy's Amazing Story",
-    "1RhbOPFSetE": "Dame Stephanie Shirley",
-    "EZpPgwaNaWI": "Billie Jean King: Coming Out",
-    "Vyn_xLrtZaY": "Margaret Heffernan",
-    "wpv1qtiwdHQ": "Mellody Hobson Commencement SC 2015",
-    "FDreaW2cEUI": "Emilia Earhart on Woman's place in science",
-    "FIN1F0TyadM": "Michelle Obama speech - International Women's Day",
-    "TIzVOwIuWvQ": "Gloria Steinem",
-    "xXM4E23Efvk": "Hillary Clinton - Womens Rights Are Human Rights",
-    "IDH4RKX428Y": "Aint I A Woman - Sojourner Truth",
-    "zcbY04JrMaU": "Virginia Woolf - Craftsmanship",
-    "8qEEqAZgULU": "Reni Eddo-Lodge: ...Talking to White People About Race",
-    "D9Ihs241zeg": "Chimamanda Ngozi Adichie - The danger of a single story",
-    "cbecKv2xR14": "Maya Angelou: Love Liberates",
-    "JMxG0reODQY": "Emma Gonzalez on guns and the NRA",
-    "IdJbJMUFzZA": "Deborah Frances-White",
-    "xAgawjzimjc": "Ashley Graham - Plus-size? More Like My Size",
-    "iauet4reDn4": "My name is Truth: the life of Sojourner Truth",
-    "z-sNlQJTBYA": "Janet Mock - Trans people are who they say they are",
-    "mM5ONxQxQu8": "Viola Davis speaks at Women's March",
-    "75WFTHpOw8Y": "Björk talking about her TV"
-};
-
-
-// Shuffle
+// Helpers
 const shuffle = a => {
     for (let i = a.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -192,39 +7,101 @@ const shuffle = a => {
     return a;
 };
 
+const createUrlParam = name => {
+    const results = new RegExp(`[/?&]${name}=([^&#]*)`).exec(window.location.href);
+    // console.log("name: "name, "results: "results);
+    return results ? results[1] : 0;
+};
+
+// TODO: get working and use to filter out missing videos
+const youtubeVideoExists = id => {
+    let videoExists;
+    const thumbnailUrl = "https://img.youtube.com/vi/" + id + "/mqdefault.jpg";
+    fetch(thumbnailUrl, {
+        mode: "no-cors"
+    }).then(response => {
+        console.log(response.status);
+        if (response.status === 404) {
+            videoExists = false;
+            console.log(`Video not found: ${id}`);
+        } else {
+            videoExists = true;
+        }
+    });
+    // .catch(response => {
+    //     console.log(response.status);
+    // });
+    return videoExists;
+};
+
+// Google Sheet data fetching
+const googleSheetUrl =
+    "https://spreadsheets.google.com/feeds/list/1d2bw9Nbhw6y4LhHVV8H3JnbFaFbT8pG8ZFX2zQyrFjI/1/public/values?alt=json";
+
+let beatArray = [],
+    speechArray = [];
+const beatIDArray = [];
+const speechIDArray = [];
+
+fetch(googleSheetUrl)
+    .then(response => response.json())
+    .then(data => {
+        const shetData = data.feed.entry;
+        shetData.forEach(sheetRow => {
+            const beatId = sheetRow.gsx$beatid.$t;
+            const beatTitle = sheetRow.gsx$beattitle.$t;
+            beatArray.push({ id: beatId, title: beatTitle });
+
+            const speechId = sheetRow.gsx$speechid.$t;
+            const speechTitle = sheetRow.gsx$speechtitle.$t;
+            speechArray.push({ id: speechId, title: speechTitle });
+        });
+
+        beatArray = beatArray.filter(video => video.id !== "");
+        speechArray = speechArray.filter(video => video.id !== "");
+
+        beatArray.forEach(beat => beatIDArray.push(beat.id));
+        speechArray.forEach(speech => speechIDArray.push(speech.id));
+    })
+    .then(() => startVideos())
+    .catch(error => console.log(error));
 
 // Global declarations
-let beatPlayer;
-let speechPlayer;
+let beatPlayer, speechPlayer;
 let isPlaying = false;
-let speechOrder;
-let beatOrder;
-let currentSpeechId;
-let currentBeatId;
-let speechKey;
-let beatKey;
+let speechOrder = [],
+    beatOrder = [];
+let currentSpeechId, currentBeatId;
+let speechKey, beatKey;
 let shareLink = window.location.origin;
 
+document.addEventListener("DOMContentLoaded", () => {
+    speechKey = createUrlParam("speech");
+    beatKey = createUrlParam("beat");
+});
 
-// Generate YouTube Players and jump to shared pair logic
-onYouTubeIframeAPIReady = () => {
-
-    if (beatKey && beatArray[beatKey]) {
-        const beatArrayCopy = Object.assign({}, beatArray);
-        delete beatArrayCopy[beatKey];
-        beatOrder = [beatKey].concat(shuffle(Object.keys(beatArrayCopy)));
+const startVideos = () => {
+    // Create playlists
+    // If url params and associated videos exist, then make those videos first
+    if (beatKey && beatArray.some(beat => beat.id === beatKey)) {
+        const beatArrayWithoutDuplicate = beatIDArray.filter(
+            beatID => beatID !== beatKey
+        );
+        beatOrder = [beatKey].concat(shuffle(beatArrayWithoutDuplicate));
     } else {
-        beatOrder = shuffle(Object.keys(beatArray));
+        beatOrder = shuffle(beatIDArray);
     }
 
-    if (speechKey && speechArray[speechKey]) {
-        const speechArrayCopy = Object.assign({}, speechArray);
-        delete speechArrayCopy[speechKey];
-        speechOrder = [speechKey].concat(shuffle(Object.keys(speechArrayCopy)));
+    if (speechKey && speechArray.some(speech => speech.id === speechKey)) {
+        const speechArrayWithoutDuplicate = speechIDArray.filter(
+            speechID => speechID !== speechKey
+        );
+        speechOrder = [speechKey].concat(shuffle(speechArrayWithoutDuplicate));
     } else {
-        speechOrder = shuffle(Object.keys(speechArray));
+        speechOrder = shuffle(speechIDArray);
     }
 
+    // Initialize YT players
     beatPlayer = new YT.Player("beatvideo", {
         width: 30,
         height: 20,
@@ -253,17 +130,11 @@ onYouTubeIframeAPIReady = () => {
         }
     });
 
-    history.replaceState({}, "Studiowave", "/" );
-
+    history.replaceState({}, "Studiowave", "/");
 };
 
-const speechInitialized = e => {
-    e.target.setPlaybackQuality("small");
-};
-const beatInitialized = e => {
-    e.target.setPlaybackQuality("small");
-};
-
+const speechInitialized = e => e.target.setPlaybackQuality("small");
+const beatInitialized = e => e.target.setPlaybackQuality("small");
 
 // titles and links
 const beatTitle = document.querySelector("#BeatTitle");
@@ -272,12 +143,12 @@ const onBeatStateChange = () => {
         event.target.setPlaybackQuality("small");
     }
     const index = beatPlayer.getPlaylistIndex();
-    // console.log("Beat playlist index: ", index);
-    const key = beatOrder[index];
-    const object = beatArray[key];
-    currentBeatId = key;
-    if (object) {
-        beatTitle.textContent = object;
+    currentBeatId = beatOrder[index];
+    const currentBeatTitle = beatArray.find(beat => beat.id === currentBeatId).title;
+    // console.log("Beat playlist item: ", index, currentBeatTitle, currentBeatId);
+
+    if (currentBeatTitle) {
+        beatTitle.textContent = currentBeatTitle;
         beatTitle.href = "https://www.youtube.com/watch?v=" + currentBeatId;
     }
 };
@@ -288,16 +159,17 @@ const onSpeechStateChange = () => {
         event.target.setPlaybackQuality("small");
     }
     const index = speechPlayer.getPlaylistIndex();
-    // console.log("Speech playlist index: ", index);
-    const key = speechOrder[index];
-    const object = speechArray[key];
-    currentSpeechId = key;
-    if (object) {
-        speechTitle.textContent = object;
-        speechTitle.href = "https://www.youtube.com/watch?v=" + currentBeatId;
+    currentSpeechId = speechOrder[index];
+    const currentSpeechTitle = speechArray.find(
+        speech => speech.id === currentSpeechId
+    ).title;
+    // console.log("Speech playlist item: ", index, currentSpeechTitle, currentSpeechId);
+
+    if (currentSpeechTitle) {
+        speechTitle.textContent = currentSpeechTitle;
+        speechTitle.href = "https://www.youtube.com/watch?v=" + currentSpeechId;
     }
 };
-
 
 // Share Clipboard
 const shareButton = document.querySelector(".sharebutton");
@@ -305,35 +177,25 @@ const tooltip = document.querySelector("#tooltip");
 
 shareButton.addEventListener("click", () => {
     if (currentBeatId && currentSpeechId) {
-        shareLink = window.location.origin + `?beat=${currentBeatId}&speech=${currentSpeechId}`;
+        shareLink =
+            window.location.origin +
+            `?beat=${currentBeatId}&speech=${currentSpeechId}`;
     }
 
-    navigator.clipboard.writeText(shareLink)
+    navigator.clipboard
+        .writeText(shareLink)
         .then(() => {
             console.log("Copied to clipboard");
-        }).catch(err => {
+        })
+        .catch(err => {
             console.error("Could not copy text: ", err);
         });
 
-    tooltip.classList.toggle("override");
+    tooltip.classList.add("override");
     setTimeout(() => {
-        tooltip.classList.toggle("override");
+        tooltip.classList.remove("override");
     }, 1000);
 });
-
-
-// URL jump to shared pair
-const urlParam = name => {
-    const results = new RegExp(`[/?&]${name}=([^&#]*)`).exec(window.location.href);
-    // console.log("name: "name, "results: "results);
-    return (results) ? results[1] : 0;
-};
-
-document.addEventListener("DOMContentLoaded", () => {
-    speechKey = urlParam("speech");
-    beatKey = urlParam("beat");
-});
-
 
 // YouTube Volume
 const beatSlider = document.querySelector("#beatslider");
@@ -342,25 +204,23 @@ const speechSlider = document.querySelector("#speechslider");
 beatSlider.onchange = () => beatPlayer.setVolume(beatSlider.value);
 speechSlider.onchange = () => speechPlayer.setVolume(speechSlider.value);
 
-
 // Play & Pause
 const playPauseButton = document.querySelector("#playpausebutton img");
 playPauseButton.addEventListener("click", () => {
     if (!beatPlayer) return;
     const beatPlayerState = beatPlayer.getPlayerState();
-    const speechPlayerState = speechPlayer.getPlayerState();
+    // const speechPlayerState = speechPlayer.getPlayerState();
     const button = document.getElementById("buttonicon");
 
     isPlaying = !isPlaying;
 
     if (isPlaying == true) {
         button.src = "images/pause.png";
-    }
-    else if (isPlaying == false) {
+    } else if (isPlaying == false) {
         button.src = "images/play.png";
     }
 
-    if (beatPlayerState == 1) {
+    if (beatPlayerState === 1) {
         beatPlayer.pauseVideo();
         speechPlayer.pauseVideo();
     } else {
@@ -368,7 +228,6 @@ playPauseButton.addEventListener("click", () => {
         speechPlayer.playVideo();
     }
 });
-
 
 // Next video
 const beatResetImage = document.querySelector("#beatreset img");
@@ -401,11 +260,14 @@ speechResetImage.addEventListener("click", () => {
     }
 });
 
-
 // if agent detected mobile: hideClass small_screen and show mobile_device
 const mobileMessage = document.querySelectorAll(".mobile_device");
 const narrowMessage = document.querySelectorAll(".small_screen");
-if ( (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i).test(navigator.userAgent) ) {
-    mobileMessage.forEach(p => p.style.display = "block");
-    narrowMessage.forEach(p => p.style.display = "none");
+if (
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+    )
+) {
+    mobileMessage.forEach(p => (p.style.display = "block"));
+    narrowMessage.forEach(p => (p.style.display = "none"));
 }
